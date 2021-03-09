6795162807a08109992dcd10e7538281b0c1b7a5
DanielJC06 <danieljeancox@gmail.com>
2021-03-09 15:37:44 -0600

    munge permision change

--- start_services.sh
+++ start_services.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
+cp /etc/munge/munge.key.tmp/MUNGE_KEY /etc/munge/munge.key
+chown munge:munge /etc/munge/munge.key
+chmod 0700 /etc/munge/munge.key
 service ssh start
 service rpcbind start
 service nfs-common start
